a44bc84fca0e3489c3ad69f8d02535dc2ed21ba0
MaxFestersen <49905988+MaxFestersen@users.noreply.github.com>
2020-04-13 14:24:17 +0200

    JS: Gem indhold der ikke skal ses når indhold indlæses.

---
+++ js/indholsvisning.js
@@ -0,0 +1,8 @@
+/* Indholdsvisning
+Formål: Skjul sektioner med JavaScript, således at formen er anvendelig uden bug af JavaScript.
+*/
+document.getElementById("pris-beregner-knap-section").classList.remove("hidden");
+document.getElementById("start").classList.add("hidden");
+document.getElementById("egenskaber").classList.add("hidden");
+document.getElementById("beregner").classList.add("hidden");
+document.getElementById("afslutning").classList.add("hidden");
