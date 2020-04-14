ad96977e7d5eaa2a3b040eeb9cf5f90d183a795c
kokojek <60446520+kokojek@users.noreply.github.com>
2020-04-14 11:58:02 +0200

    Update indholsvisning.js

--- js/indholsvisning.js
+++ js/indholsvisning.js
@@ -4,3 +4,16 @@ document.getElementById("start").classList.add("hidden");
 document.getElementById("egenskaber").classList.add("hidden");
 document.getElementById("beregner").classList.add("hidden");
 document.getElementById("afslutning").classList.add("hidden");
+/*Denne kode lader brugeren at bevæge sig mellem beregneres trin vedd at klippe på "fortsæt"/"tilbage" knapper og/eller breadcrumbs*/
+function showHideSection(sectionToBeShown){	
+		var ids = ["pris-beregner-knap-section", "start", "egenskaber", "beregner", "resultat", "afslutning"]
+		
+		for(i = 0; i < ids.length; i++){
+			if(sectionToBeShown == ids[i]){
+				document.getElementById(ids[i]).classList.remove("hidden");
+			} 
+			else {
+				document.getElementById(ids[i]).classList.add("hidden");
+			}
+		}
+	}
