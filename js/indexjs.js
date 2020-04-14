971a98abe1cb271bf1c1d293d1809a45c1c5a7d5
kokojek <60446520+kokojek@users.noreply.github.com>
2020-04-14 12:58:03 +0200

    Update indexjs.js

---
+++ js/indexjs.js
@@ -0,0 +1,11 @@
+/*Js for scrollview*/ 
+function scroll_to(id) {
+      document.documentElement.style.scrollBehavior = 'smooth'
+      element = document.createElement('a');
+      element.setAttribute('href', id)
+      element.click();
+  }
+
+  document.getElementById("tocontent").addEventListener('click', () => {
+  scroll_to('#7')
+  });
