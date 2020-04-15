750dea133a53a821b701235d48bf0b2954ee9e6a
Miex2157 <miex2157@edu.ucl.dk>
2020-04-15 11:51:50 +0200

    scrolltobeshown

--- js/indholsvisning.js
+++ js/indholsvisning.js
@@ -5,15 +5,18 @@ document.getElementById("egenskaber").classList.add("hidden");
 document.getElementById("beregner").classList.add("hidden");
 document.getElementById("afslutning").classList.add("hidden");
 /*Denne kode lader brugeren at bevæge sig mellem beregneres trin vedd at klippe på "fortsæt"/"tilbage" knapper og/eller breadcrumbs*/
-function showHideSection(sectionToBeShown){	
+function showHideSection(sectionToBeShown){
 	var ids = ["pris-beregner-knap-section", "start", "egenskaber", "beregner", "resultat", "afslutning"]
-	
+
 	for(i = 0; i < ids.length; i++){
 		if(sectionToBeShown == ids[i]){
 			document.getElementById(ids[i]).classList.remove("hidden");
-		} 
+		}
 		else {
 			document.getElementById(ids[i]).classList.add("hidden");
 		}
 	}
+	document.getElementById(sectionToBeShown).scrollIntoView({
+	behavior: 'smooth'
+	});
 }
